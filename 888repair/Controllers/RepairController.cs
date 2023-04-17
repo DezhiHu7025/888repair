d79218f428e1118e8943d25807a6aa01ef75711d
ivy <2023180742@qq.com>
2023-04-17 13:55:14 +0800

    IT资讯类填写问题v2

--- 888repair/Controllers/RepairController.cs
+++ 888repair/Controllers/RepairController.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using _888repair.Db;
+using _888repair.Models;
+using _888repair.Models.Repair;
+using _888repair.Service;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -31,5 +37,112 @@ namespace _888repair.Controllers
         {
             return View();
         }
+
+        public ActionResult ITRepairSave(RepairRecordModel model)
+        {
+            try
+            {
+                string sql = "";
+                model.RepairId = DateTime.Now.ToShortTimeString();
+                model.RepairId = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                model.ResponseEmpno = "H22080031";
+                model.ResponseEmpname = "dezhi_hu";
+                model.CreatTime = DateTime.Now;
+                model.Status = "";
+                if (!string.IsNullOrEmpty(model.PhotoPath))
+                {
+                    model.PhotoPath = model.PhotoPath.Substring(0, model.PhotoPath.Length - 1);
+                    string[] photos = model.PhotoPath.Split(';');
+                    string paths = null;
+                    foreach (var photo in photos)
+                    {
+                        paths += "/UploadFile/" + photo + ";";
+                    }
+                    model.PhotoPath = paths;
+                }
+                using (RepairDb db = new RepairDb())
+                {
+                    var chargeModel = new DirectorModel();
+                    //资讯类找顺位为1的辖区负责人  后勤类找顺位为1的业管负责人
+                    //抓取负责人工号及姓名
+                    if (model.SystemCategory == "IT(资讯类)")
+                    {
+                        string findChargeSql = @"SELECT a.charge_emp EmpNo,
+                                                        b.FullName
+                                                FROM[888_KsNorth].[dbo].[match] a
+                                                LEFT JOIN[888_KsNorth].[dbo].[charge] b
+                                                ON a.charge_emp = b.EmpNo
+                                                WHERE match_type = 'AreaMatch'
+                                                      AND area_id = @AreaId
+                                                      AND sort = '1' ";
+                        chargeModel = db.Query<DirectorModel>(findChargeSql, new { AreaId = model.AreaId }).FirstOrDefault();
+                        model.ChargeEmpno = chargeModel.EmpNo;
+                        model.ChargeEmpname = chargeModel.FullName;
+                    }
+                    else if (model.SystemCategory == "Logistics(总务后勤类)")
+                    {
+                        string findChargeSql = @"SELECT a.charge_emp EmpNo,
+                                                        b.FullName
+                                                FROM[888_KsNorth].[dbo].[match] a
+                                                LEFT JOIN[888_KsNorth].[dbo].[charge] b
+                                                ON a.charge_emp = b.EmpNo
+                                                WHERE match_type = 'KindMatch'
+                                                      AND area_id = @AreaId
+                                                      AND sort = '1' ";
+                        chargeModel = db.Query<DirectorModel>(findChargeSql, new { AreaId = model.AreaId }).FirstOrDefault();
+                    }
+
+                    if (chargeModel == null)
+                    {
+                        return Json(new FlagTips { IsSuccess = false, Msg = "负责人抓取异常，请联系资讯" });
+
+                    }
+                    sql = @"insert into [888_KsNorth].[dbo].[record] (repair_id,area_id,kind_id,SystemCategory,Building,Loaction,
+                                               charge_empno,charge_empname,Category,ResponseContent,ReplyContent,Status,CreatTime,
+                                               PhotoPath,RoomNum,repairTime,Telephone,DamageReason,DamageClass,DamageName,ResponseEmpno,
+                                               ResponseEmpname,FinishTime) 
+                                       values (@RepairId,@AreaId,@KindId,@SystemCategory,@Building,@Loaction,
+                                               @ChargeEmpno,@ChargeEmpname,@Category,@ResponseContent,@ReplyContent,@Status,@CreatTime,
+                                               @PhotoPath,@RoomNum,@RepairTime,@Telephone,@DamageReason,@DamageClass,@DamageName,@ResponseEmpno,
+                                               @ResponseEmpname,@FinishTime)";
+                    Dictionary<string, object> trans = new Dictionary<string, object>();
+                    trans.Add(sql, model);
+                    db.DoExtremeSpeedTransaction(trans);
+                }
+            }
+            catch (Exception ex)
+            {
+                FileUploadService upfile = new FileUploadService();
+                string result = upfile.ITRepairPicDelete(model.PhotoPath);
+                if (result != "OK")
+                {
+                    return Json(new FlagTips { IsSuccess = false, Msg = "图片处理发生异常，请刷新页面重新提交表单" });
+                }
+                return Json(new FlagTips { IsSuccess = false, Msg = ex.Message });
+            }
+            return Json(new FlagTips { IsSuccess = true });
+        }
+
+        public ActionResult ITRepairPicUpload()
+        {
+            HttpPostedFileBase httpPostedFileBase = Request.Files["file"];
+            ControllerContext.HttpContext.Request.ContentEncoding = Encoding.GetEncoding("UTF-8");
+            ControllerContext.HttpContext.Response.Charset = "UTF-8";
+            RepairRecordModel model = new RepairRecordModel();
+            string Stu_Empno = "H22080031";
+            if (httpPostedFileBase != null)
+            {
+                FileUploadService upfile = new FileUploadService();
+                var uploadModel = upfile.FileLoad(httpPostedFileBase, Stu_Empno);
+                if (!uploadModel.IsSuccess)
+                {
+                    return Json(new FlagTips { IsSuccess = false, Msg = "照片上传失败 Photo upload failed" });
+                }
+
+            }
+            return Json(new FlagTips { IsSuccess = true });
+        }
+
+
     }
 }
---
+++ 888repair/Models/Repair/RepairRecordModel.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _888repair.Models.Repair
+{
+    public class RepairRecordModel
+    {
+        /// <summary>
+        /// 请修编号
+        /// </summary>
+        public string RepairId { get; set; }
+
+        /// <summary>
+        /// 辖区Id
+        /// </summary>
+        public string AreaId { get; set; }
+
+        /// <summary>
+        /// 业管Id
+        /// </summary>
+        public string KindId { get; set; }
+
+        /// <summary>
+        /// 系统类别
+        /// </summary>
+        public string SystemCategory { get; set; }
+
+        /// <summary>
+        /// 大楼别
+        /// </summary>
+        public string Building { get; set; }
+
+        /// <summary>
+        /// 位置
+        /// </summary>
+        public string Loaction { get; set; }
+
+        /// <summary>
+        /// 负责人员工编号
+        /// </summary>
+        public string ChargeEmpno { get; set; }
+
+        /// <summary>
+        /// 负责人姓名
+        /// </summary>
+        public string ChargeEmpname { get; set; }
+
+        /// <summary>
+        /// 反应人员工编号
+        /// </summary>
+        public string ResponseEmpno { get; set; }
+
+        /// <summary>
+        /// 反应人员工姓名
+        /// </summary>
+        public string ResponseEmpname { get; set; }
+
+        /// <summary>
+        /// 类别
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// 反应内容
+        /// </summary>
+        public string ResponseContent { get; set; }
+
+        /// <summary>
+        /// 回复内容
+        /// </summary>
+        public string ReplyContent { get; set; }
+
+        /// <summary>
+        /// 执行状态
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// 填表时间
+        /// </summary>
+        public DateTime? CreatTime { get; set; }
+
+        /// <summary>
+        /// 完成时间
+        /// </summary>
+        public DateTime? FinishTime { get; set; }
+
+        /// <summary>
+        /// 照片位置
+        /// </summary>
+        public string PhotoPath { get; set; }
+
+        /// <summary>
+        /// 空间编号
+        /// </summary>
+        public string RoomNum { get; set; }
+
+        /// <summary>
+        ///维修时段
+        /// </summary>
+        public string RepairTime { get; set; }
+
+        /// <summary>
+        /// 分机号码
+        /// </summary>
+        public string Telephone { get; set; }
+
+        /// <summary>
+        /// 破坏原因
+        /// </summary>
+        public string DamageReason { get; set; }
+
+        /// <summary>
+        /// 破坏人所属单位
+        /// </summary>
+        public string DamageClass { get; set; }
+
+        /// <summary>
+        /// 破坏人姓名
+        /// </summary>
+        public string DamageName { get; set; }
+        
+    }
+}
