743f82c6c84235a258facd6287f9e566c0840b97
ivy <2023180742@qq.com>
2023-06-01 13:24:19 +0800

    移动端首页

--- 888repair/Controllers/HomeController.cs
+++ 888repair/Controllers/HomeController.cs
@@ -14,6 +14,11 @@ namespace _888repair.Controllers
             return View();
         }
 
+        public ActionResult MobileIndex()
+        {
+            return View();
+        }
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
